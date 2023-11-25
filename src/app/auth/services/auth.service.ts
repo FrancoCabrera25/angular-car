a1b5aa55c2b366a459573c0a9eaf4ca540c5eed4
Franco Cabrera <fcabrera.e@cocha.com>
2023-11-24 22:26:05 -0300

    auth

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, catchError, map, tap } from 'rxjs';
+import { Injectable, computed, signal } from '@angular/core';
+import { Observable, catchError, map, tap, throwError } from 'rxjs';
 import { environments } from 'src/environments/environments';
 import { User } from '../interface/user.interface';
+import { AuthStatus } from '../interface/auth-status.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -11,18 +12,26 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   private baseUrl = environments.baseUrl;
-  private _currentUser?: User;
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
-  get currentUser(): User | undefined {
-    return this._currentUser;
-  }
+  public currentUser = computed(() => this._currentUser());
+  public authStatus = computed(() => this._authStatus());
+
   public login(email: string, password: string): Observable<User> {
     return this.http
       .post<User>(`${this.baseUrl}/auth/login`, { email, password })
       .pipe(
         tap((user) => console.log('user', user)),
-        tap((user) => (this._currentUser = user)),
-        tap((user) => localStorage.setItem('token', user.token))
+        tap((user) => {
+          this._currentUser.set(user);
+          this._authStatus.set(AuthStatus.authenticated);
+          localStorage.setItem('token', user.token);
+        }),
+        catchError(() => {
+          this._authStatus.set(AuthStatus.notAuthenticated);
+          return throwError(() => 'Error login');
+        })
       );
   }
 
